5fdb51f424d94559dc8bbd8969b06ff372a3e8ac
Bossfroi <froisidmer@gmail.com>
2023-11-06 02:38:34 +0800

    23

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Home/",
   server: {
-    host: "mini2-frontend2.onrender.com", // Change this to the desired localhost address
     port: 80, // Change this to the desired port number
   },
 });
