b4c2870202bf38ecf3a1b0f04c5239bac281d9fe
Bossfroi <froisidmer@gmail.com>
2023-11-06 02:19:31 +0800

    6-11-23

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-router/",
+  base: "/Home/",
+  server: {
+    port: 8080, // Change this to the desired port number
+  },
 });
